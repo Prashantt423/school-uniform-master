7d5e7e420856010b55af433a646e81fa87699bc7
Prashantt423 <84055497+Prashantt423@users.noreply.github.com>
2022-03-17 17:16:27 +0530

    first commit

---
+++ school-uniform-master/src/components/category/Category.tsx
@@ -0,0 +1,74 @@
+import "./category.scss"
+export default function Category() {
+    return (
+        <div className="cardBox">
+            <div className="categoryGrid">
+                <div className="leftCategoryItems">
+                    <div className="phone">
+                        <div className="categoryBox">
+                            <div className="categoryTitle">
+                                <span className="categoryCardTitle">Phone</span>
+                                <span className="categoryCardDesc">3,482 items</span>
+                            </div>
+                            <button className="seeAll">see All</button>
+                        </div>
+                    </div>
+                    <div className="flexRow">
+                        <div className="watch">
+                            <div className="categoryBox">
+                                <div className="categoryTitle">
+                                    <span className="categoryCardTitle">Watch</span>
+                                    <span className="categoryCardDesc">3,482 items</span>
+                                </div>
+                                <button className="seeAll">see All</button>
+                            </div>
+                        </div>
+                        <div className="headphone">
+                            <div className="categoryBox">
+                                <div className="categoryTitle">
+                                    <span className="categoryCardTitle">Headphone</span>
+                                    <span className="categoryCardDesc">3,482 items</span>
+                                </div>
+                                <button className="seeAll">see All</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="rightCategoryItems">
+                    <div className="flexRow">
+                        <div className="tablet">
+                            <div className="categoryBox">
+                                <div className="categoryTitle">
+                                    <span className="categoryCardTitle">Tablet</span>
+                                    <span className="categoryCardDesc">3,482 items</span>
+                                </div>
+                                <button className="seeAll">see All</button>
+                            </div></div>
+                        <div className="laptop">
+
+                            <div className="categoryBox">
+                                <div className="categoryTitle">
+                                    <span className="categoryCardTitle">Laptop</span>
+                                    <span className="categoryCardDesc">3,482 items</span>
+                                </div>
+                                <button className="seeAll">see All</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="accessories">
+                        <div className="categoryBox">
+                            <div className="categoryTitle">
+                                <span className="categoryCardTitle">Accessories</span>
+                                <span className="categoryCardDesc">3,482 items</span>
+                            </div>
+                            <button className="seeAll">see All</button>
+                        </div>
+                    </div>
+
+                </div>
+
+
+            </div>
+        </div>
+    )
+}
